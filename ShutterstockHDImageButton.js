9734c6d66556292c627f6df53699439c5391b948
danilluzin <danilluzin@gmail.com>
2023-06-17 21:19:36 +0200

    migrate from browser. preview button works.

--- ShutterstockHDImageButton.js
+++ ShutterstockHDImageButton.js
@@ -8,114 +8,94 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shutterstock.com
 // @grant        GM_addStyle
 // @require https://code.jquery.com/jquery-3.6.0.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
+console.log("start");
 
-(function () {
-	'use strict';
-	//mui-18xza3-root
-	//mui-b5j3lh-item-sstkGridItem-item
-	$(".mui-b5j3lh-item-sstkGridItem-item").each(
-		function (index) {
-			//var targetUrl = $(this).children('.mui-t7xql4-a-inherit-link').attr('href');
-			var targetUrl = "uuuurl";
-			var button = document.createElement("Button");
-			button.innerHTML = "me" + targetUrl;
-			button.style = " z-index: 1000000;position: relative";
-			this.appendChild(button);
-			//var=
+waitForKeyElements(".mui-b5j3lh-item-sstkGridItem-item", injector);
+waitForKeyElements(".mui-1p72qfc-item-sstkGridItem", injector);
 
-			//target url to reconstruct :https://www.shutterstock.com/shutterstock/photos/709438852/display_1500/f.jpg
-			/*  this.append ( `
-			  <div id="gmSomeID">
-			  me: `+index +`
-			  </div>
-			  ` );
-			  */
+$("head").append(`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">`);
+$("head").append(
+	`<style>
+     .hide {
+       display: none;
+     }
 
-			/* this.append( `
-    <div id="gmSomeID">
-        <p>Some paragraph</p>
-        etc.
-    </div>
-`);*/
-		}
-	);
+    .injected:hover .hide {
+      display: block;
+      color: red;
+    }
+     </style>
+    `
+);
 
-	$("body").append(`
-    <div id="gmSomeID">
-        <p>u wot mate</p>
-        etc.
-    </div>
-`);
+function injector(jNode) {
+	console.log("waited for :3");
+	$(jNode).hover(
+		function (e) {
+			console.log("hover");
+			var isInjected = $(this).attr('injected');
+			console.log("injected = " + isInjected);
+			if (isInjected !== undefined && isInjected === "injected") {
+				console.log("gotout");
+				return;
+			}
+			console.log("injecting");
+			$(this).attr('injected', "injected");
+			$(this).addClass("injected");
+			var targetUrl = $(this).children('.mui-t7xql4-a-inherit-link').attr('href');
+			if (targetUrl === undefined) {
+				return;
+			}
+			//var targetUrl = "uuuurl";
+			//            targetUrl = "https://www.shutterstock.com/shutterstock/photos/1326079727/display_1500/stock-photo-ancient-stonework-in-the-night-1326079727.jpg"
+			// we get - https://www.shutterstock.com/image-photo/ancient-stonework-night-1326079727
+			var imageName = targetUrl.substring(targetUrl.lastIndexOf('/') + 1);
+			var imageID = imageName.split('-').slice(-1)[0];
+			var hdlink = document.createElement("a");
+			$(hdlink).addClass("hide");
+			var hdlinkUrl = "https://www.shutterstock.com/shutterstock/photos/" + imageID + "/display_1500/" + imageName + ".jpg";
+			//         hdlink.innerHTML = "id="+imageID+" name="+imageName;
+			hdlink.innerHTML = `<i class="fa-regular fa-file-image fa-xl" style="color: #ffffff;"></i>`;
+			//         hdlink.innerHTML =`<i class="fa-solid fa-file-arrow-down fa-xl" style="color: #ffffff;"></i>`;
+			hdlink.setAttribute('href', hdlinkUrl);
+			hdlink.setAttribute('target', "_blank");
+			hdlink.style = ` z-index: 1000000;
+                          position: absolute;
+                          top:40pt;
+                          right:20pt;
+
+                          padding: 8pt;
+                          border-radius: 10pt;
+                          font-size:12pt;
+                          background: rgba(0,0,0,0.4);`;
+
+			this.appendChild(hdlink);
+		});
+}
 
 
-	$(".mui-b5j3lh-item-sstkGridItem-item").each(function (index) {
-		this.append(`
+function injectIntoMain() {
+	//".mui-1t9dezy-root-blurredImageBackground-backgroundWithActions"
+	$(".mui-1t9dezy-root-blurredImageBackground-backgroundWithActions").append(`
     <div id="gmSomeID">
         <p>wot</p>
         etc.
     </div>
 `);
-	});
+}
+
+
+
+(function () {
+	'use strict';
+
+	$("body").append(`
+    <div id="gmSomeID">
+        <p>wot</p>
+        etc.
+    </div>
+`);
 
 })();
-
-
-
-$('.mui-b5j3lh-item-sstkGridItem-item').hover(function (e) {
-	console.log("hover");
-	$(this).trigger('custom');
-});
-
-$(document).on('custom', function (e) {
-	console.log('document is handling custom event triggered by ' + e.target.id);
-});
-
-
-console.log("start");
-
-/* Define helper functions */
-var processAnchor = function (a) {
-	//if (a.hasAttribute('target')) {
-	//    a.removeAttribute('target');
-	//}
-	//    a.setAttribute('target', '_self');
-	console.log("modifed");
-
-};
-
-/* Define the observer for watching over inserted elements */
-var insertedObserver = new MutationObserver(function (mutations) {
-	mutations.forEach(function (m) {
-		var inserted = [].slice.call(m.addedNodes);
-		while (inserted.length > 0) {
-			var elem = inserted.shift();
-			[].slice.call(elem.children || []).forEach(function (el) {
-				inserted.push(el);
-			});
-			if (elem.nodeName === 'A') {
-				processAnchor(elem);
-			}
-		}
-	});
-});
-
-/* Define the observer for watching over
- * modified attributes of anchor elements */
-var modifiedObserver = new MutationObserver(function (mutations) {
-	mutations.forEach(function (m) {
-		if ((m.type === 'attributes') && (m.target.nodeName === 'A')) {
-			processAnchor(m.target);
-		}
-	});
-});
-
-/* Start observing */
-insertedObserver.observe(document.documentElement, {
-	childList: true,
-	subtree: true
-});
-modifiedObserver.observe(document.documentElement, {
-	attributes: true,
-	substree: true
-});
